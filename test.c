03a38a922716379c7c3b9b849935de78c4413c44
Adrien <adrienrossignol1@gmail.com>
2020-09-12 20:59:23 +0200

    Update

--- test.c
+++ test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sgardner <stephenbgardner@gmail.com>       +#+  +:+       +#+        */
+/*   By: adrossig <adrossig@students.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/28 20:51:23 by sgardner          #+#    #+#             */
-/*   Updated: 2017/10/18 22:40:49 by sgardner         ###   ########.fr       */
+/*   Updated: 2020/09/12 20:54:13 by adrossig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ static void	print_report(int count)
 			g_num_mallocs - count);
 	fprintf(stderr, "%-26s %d\n", "Data structures freed:",
 			g_num_frees - count);
-	print_alloc_report(1);
+	print_alloc_report(0);
 }
 
 int			main(int argc, char **argv)
@@ -35,9 +35,9 @@ int			main(int argc, char **argv)
 	int		count;
 	int		i;
 
-	fprintf(stderr,	"\nRunning %d iterations with BUFF_SIZE %d...\n",
-			ITER, BUFF_SIZE);
-	fprintf(stderr, "Increase BUFF_SIZE or decrease ITER in run_test.sh if"
+	fprintf(stderr,	"\nRunning %d iterations with BUFFER_SIZE %d...\n",
+			ITER, BUFFER_SIZE);
+	fprintf(stderr, "Increase BUFFER_SIZE or decrease ITER in run_test.sh if"
 			" test takes too long.\n");
 	i = 0;
 	while (i++ < ITER)
@@ -51,7 +51,7 @@ int			main(int argc, char **argv)
 			fd = 0;
 		line = NULL;
 		count = 0;
-		while (get_next_line(fd, &line) > 0)
+		while ((get_next_line(fd, &line)) == 1)
 		{
 			printf("%s\n", line);
 			free(line);
